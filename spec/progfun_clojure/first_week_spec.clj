e96ff63e0034577324748e55717111e127f311aa
Pepe García <jl.garhdez@gmail.com>
2014-05-07 23:16:19 +0200

    finished the first problem.

--- spec/progfun_clojure/first_week_spec.clj
+++ spec/progfun_clojure/first_week_spec.clj
@@ -1,4 +1,17 @@
 (ns progfun-clojure.firs_week_spec
-  (:require [speclj.core :refer :all]))
+  (:require [progfun_clojure.first_week :refer :all]
+            [speclj.core :refer :all]))
+
+(describe "pascal triangle"
+
+  (it "tests if the an incorrect call returns nil"
+    (should (nil? (pascal 2 3)))
+    (should (nil? (pascal 2 3))))
+
+  (it "tests if the pascal function returns correct numbers"
+    (should= 2 (pascal 2 1))
+    (should= 3 (pascal 3 1))
+    (should= 4 (pascal 4 1))
+    (should= 3 (pascal 3 2))))
 
 (run-specs)
--- src/progfun_clojure/first_week.clj
+++ src/progfun_clojure/first_week.clj
@@ -1,1 +1,14 @@
 (ns progfun_clojure.first_week)
+
+(defn pascal [rows columns]
+  "
+  Returns the value of a position in the Pascal Triangle given a row and a column
+  "
+  (if (> columns rows)
+    nil
+    (loop [rows-copy rows
+           columns-copy columns
+           acc 1]
+      (if (or (= rows-copy columns-copy) (= columns-copy 0) (= rows-copy 0))
+        acc
+        (recur (dec rows-copy) (dec columns-copy) (/ (* acc rows-copy) columns-copy))))))
