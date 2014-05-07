e09eca6fec498752f8fe46e5ea586f89c614cb15
Pepe García <jl.garhdez@gmail.com>
2014-05-07 21:36:41 +0200

    Initial commit

---
+++ test/progfun_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns progfun-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [progfun-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
